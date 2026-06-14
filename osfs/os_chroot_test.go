3bdde00f085ef13c8c9edd62c19558dcaa71753d
Seal <lev.p@sealsecurity.io>
2026-06-14 16:13:52 +0000

    patched v5.6.2

--- osfs/os_chroot_test.go
+++ osfs/os_chroot_test.go
@@ -51,6 +51,40 @@ func (s *ChrootOSSuite) TestOpenDoesNotCreateDir(c *C) {
 	c.Assert(os.IsNotExist(err), Equals, true)
 }
 
+func (s *ChrootOSSuite) TestEmptyBaseChrootPreservesAbsolutePath(c *C) {
+	dir := c.MkDir()
+	c.Assert(os.Mkdir(filepath.Join(dir, ".git"), 0755), IsNil)
+
+	fs, err := newChrootOS("").Chroot(dir)
+	c.Assert(err, IsNil)
+	c.Assert(fs.Root(), Equals, dir)
+
+	fi, err := fs.Stat(".git")
+	c.Assert(err, IsNil)
+	c.Assert(fi.IsDir(), Equals, true)
+}
+
+func (s *ChrootOSSuite) TestSymlinkedRoot(c *C) {
+	root := c.MkDir()
+	realRoot := filepath.Join(root, "real")
+	linkRoot := filepath.Join(root, "link")
+
+	c.Assert(os.Mkdir(realRoot, 0755), IsNil)
+	if err := os.Symlink(realRoot, linkRoot); err != nil {
+		c.Skip("symlink creation is not supported")
+	}
+
+	fs := newChrootOS(linkRoot)
+
+	fi, err := fs.Stat("/")
+	c.Assert(err, IsNil)
+	c.Assert(fi.IsDir(), Equals, true)
+
+	_, err = fs.Stat(".git")
+	c.Assert(os.IsNotExist(err), Equals, true)
+	c.Assert(err, Not(Equals), billy.ErrCrossedBoundary)
+}
+
 func (s *ChrootOSSuite) TestCapabilities(c *C) {
 	_, ok := s.FS.(billy.Capable)
 	c.Assert(ok, Equals, true)
